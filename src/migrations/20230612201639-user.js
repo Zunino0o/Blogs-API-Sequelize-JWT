c047efbd0992f37b42f4d7762f4b83c93a660388
Lucas Zunino <zunino10@hotmail.com>
2023-06-12 17:49:56 -0300

    adiciona migrations

---
+++ src/migrations/20230612201639-user.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', { 
+      id: {
+        type: Sequelize.INTEGER
+      },
+      display_name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
