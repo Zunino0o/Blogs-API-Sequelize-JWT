7dd053c8e2e45701a1a50ad640328956dcddd623
Lucas Zunino <zunino10@hotmail.com>
2023-06-12 19:02:04 -0300

    req11 done!

--- src/models/PostCategory.js
+++ src/models/PostCategory.js
@@ -3,6 +3,18 @@ module.exports = (sequelize, DataTypes) => {
       post_id: DataTypes.INTEGER,
       category_id: DataTypes.INTEGER,
     });
+    PostCategory.associate = (models) => {
+        models.BlogPost.belongsToMany(models.Category, {
+            through: PostCategory,
+            foreignKey: "post_id",
+            otherKey: "category_id"
+        });
+        models.Category.belongsToMany(models.BlogPost, {
+            through: PostCategory,
+            foreignKey: "category_id",
+            otherKey: "post_id"
+        });
+    }
     return PostCategory;
   };
   
