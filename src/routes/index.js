bf5ff1a974356c2c4dba4a179b5fada4d9c26aa6
Lucas Zunino <zunino10@hotmail.com>
2023-06-13 16:05:38 -0300

    termina esqueleto dos routers

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+const categoryRouter = require('./categoryRouter');
+const loginRouter = require('./loginRouter');
+const postRouter = require('./postRouter');
+const userRouter = require('./userRouter');
+
+module.exports = {
+    categoryRouter,
+    loginRouter,
+    postRouter,
+    userRouter,
+};
